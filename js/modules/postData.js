5b04835464681e783ad5bce1668e5060809d81e4
ogonnamatty1 <ogonnamatthew@gmail.com>
2023-08-27 20:23:55 +0100

    changes

--- js/modules/postData.js
+++ js/modules/postData.js
@@ -1,8 +1,29 @@
- async function postusers(url, data) {
+//  async function postusers(url, data) {
+//     // Default options are marked with *
+//     const response = await fetch(url, {
+//       method: "POST", // *GET, POST, PUT, DELETE, etc.
+//       mode: "no-cors", // no-cors, *cors, same-origin
+//     //   cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+//       credentials: "same-origin", // include, *same-origin, omit
+//       headers: {
+//         "Content-Type": "application/json",
+//         // 'Content-Type': 'application/x-www-form-urlencoded',
+//       },
+//     //   redirect: "follow", // manual, *follow, error
+//       referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+//       body: JSON.stringify(data), // body data type must match "Content-Type" header
+//     });
+//     return await response.json().then(data2 => data2); // parses JSON response into native JavaScript objects
+//   }
+     
+// export default postusers
+
+async function postusers(url, data) {
+  try {    
     // Default options are marked with *
-    const response = await fetch(url, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "no-cors", // no-cors, *cors, same-origin
+    const response =  await fetch(url, {
+      method: "POSTgit ", // *GET, POST, PUT, DELETE, etc.
+      // mode: "cors", // no-cors, *cors, same-origin
     //   cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
       credentials: "same-origin", // include, *same-origin, omit
       headers: {
@@ -13,8 +34,12 @@
       referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: JSON.stringify(data), // body data type must match "Content-Type" header
     });
-    return response.json(); // parses JSON response into native JavaScript objects
+    console.log('response.json()', response.json());
+    return response.json().catch(res => res); // parses JSON response into native JavaScript objects
+  } catch (error) {
+    throw new Error(error.message, error)
   }
-     
+}
+   
 export default postusers
   
--- validation.js
+++ validation.js
@@ -1,6 +1,132 @@
+// // import { request } from 'axios';
+// // import postusers  from './js/modules/postData.js';
+
+// const urlApply = "https://timat-backend.vercel.app/api/timatech/apply";
+// const form = document.querySelector('#form');
+// const frontendResponse = document.querySelector('.api_response')
+// const whatsappGroup = document.querySelector('.whatsapp-group>a')
+
+
+
+// function addNewStudent(studentData, url) {
+
+//     // let axiosdata = JSON.stringify(studentData);
+
+//     // let config = {
+//     //     method: 'post',
+//     //     maxBodyLength: Infinity,
+//     //     url: 'https://timat-backend.vercel.app/api/timatech/apply',
+//     //     headers: { 
+//     //         'Content-Type': 'application/json'
+//     //     },
+//     //     data : axiosdata
+//     // };
+
+//     // request(config).then((response) => {
+//     //     return JSON.stringify(response.data);
+//     // })
+//     // .catch((error) => {
+//     //     console.log(error);
+//     // });
+
+
+//     var myHeaders = new Headers();
+// myHeaders.append("Content-Type", "application/json");
+
+// var raw = JSON.stringify(studentData);
+
+// var requestOptions = {
+//   method: 'POST',
+//   headers: myHeaders,
+//   body: raw,
+//   redirect: 'follow',
+// };
+
+// fetch("http://localhost:5000/api/timatech/apply/", {...requestOptions, mode: 'no-cors',
+//     headers: {
+//         "Content-Type": "application/json",
+//         'Accept': 'application/json, text/plain, */*',
+//         credentials: "omit", //
+//         // "Content-Type": "application/x-www-form-urlencoded",
+//     },
+// })
+//   .then(response => response)
+//   .catch(error => console.log('error', error));
+// }
+
+
+// // async function _postData(url = '', data = {}) {
+// //     const response = await fetch(url, {
+// //         method: 'POST',
+// //         mode: 'cors',
+// //         cache: 'no-cache',
+// //         credentials: 'same-origin',
+// //         redirect: 'follow',
+// //         referrerPolicy: 'no-referrer',
+// //         headers: {
+// //             "Content-type": "application/json; charset=UTF-8"
+// //         },
+// //         body: JSON.stringify(data)
+// //     });
+    
+// //     return response.json();
+// // }
+
+
+
+
+// form.addEventListener('submit', async (e)=>{
+   
+//     e.preventDefault();
+//     const name = document.querySelector('#name').value;
+//     const phone = document.querySelector('#phone').value;
+//     const email = document.querySelector('#email').value;
+//     const userData = {name, phone, email };
+   
+    
+//     console.log('working');
+//     // const handleRegister = await fetch(url, {
+//     //     method:'POST',
+//     //     headers:{
+//     //         "Content-Type":"application/json"
+//     //     },
+//     //     body:JSON.stringify({name, phone, email, }),
+//     //     return handleRegister;
+//     // })
+//     // console.log('handleRegister', handleRegister);
+//    console.log("userdata", userData);
+//    console.log("userdata json", JSON.stringify(userData));
+//    addNewStudent(userData)
+// //    _postData('http://127.0.0.1:8080', userData)
+//     //   try {
+//         //    postusers(urlApply, userData)
+//         //    .then((res) => {
+//         //         console.log(res?.message); // JSON data parsed by `data.json()` call
+//         //         if((res?.message === 'E-mail already exist') || (res?.message === "Phone number already exist") ||(res?.message ==="Invalid phone number")) {
+//         //             frontendResponse.style.color = 'red'
+//         //             console.log(frontendResponse);
+//         //         } else {
+//         //             frontendResponse.style.color = 'green'
+//         //             whatsappGroup.textContent= 'Click to join our whatsapp group'
+//         //             setTimeout(()=> {frontendResponse.textContent="", form.reset()}, 5000)
+//         //         }
+//         //         return frontendResponse.textContent = res?.message
+                
+//         //     });
+            
+//         // } catch (error) {
+//         //     return frontendResponse.textContent = 'Network issue contact your network provider'
+//         // }
+       
+// }) 
+
+
+
+
 
 import postusers  from './js/modules/postData.js';
-const urlApply = "https://timat-backend.vercel.app/api/timatech/apply";
+const urlApply = "http://127.0.0.1:8000/api/timatech/apply";
+
 const form = document.querySelector('#form');
 const frontendResponse = document.querySelector('.api_response')
 const whatsappGroup = document.querySelector('.whatsapp-group>a')
@@ -12,10 +138,11 @@ form.addEventListener('submit', async (e)=>{
     const name = document.querySelector('#name').value;
     const phone = document.querySelector('#phone').value;
     const email = document.querySelector('#email').value;
-    const userData = {name, phone, email };
+    
+    const data = {name, phone, email }
    
     
-    console.log('working');
+    // console.log('working');
     // const handleRegister = await fetch(url, {
     //     method:'POST',
     //     headers:{
@@ -27,14 +154,14 @@ form.addEventListener('submit', async (e)=>{
     // console.log('handleRegister', handleRegister);
    
       try {
-           postusers(urlApply, userData).then((res) => {
+           postusers(urlApply, data).then((res) => {
                 console.log(res?.message); // JSON data parsed by `data.json()` call
-                if((res?.message === 'E-mail already exist') || (res?.message === "Phone number already exist") ||(res?.message ==="Invalid phone number")) {
+                if((res?.message === 'email already exist') || (res?.message === "phone number already exist") ||(res?.message ==="invalid phone number")) {
                     frontendResponse.style.color = 'red'
                     console.log(frontendResponse);
                 } else {
                     frontendResponse.style.color = 'green'
-                    whatsappGroup.textContent= 'Click to join our whatsapp group'
+                    whatsappGroup.textContent= 'click to join whatsapp group'
                     setTimeout(()=> {frontendResponse.textContent="", form.reset()}, 5000)
                 }
                 return frontendResponse.textContent = res?.message
@@ -42,7 +169,7 @@ form.addEventListener('submit', async (e)=>{
             });
             
         } catch (error) {
-            return frontendResponse.textContent = 'Network issue contact your network provider'
+            return frontendResponse.textContent = 'network issue contact your network provider'
         }
        
 }) 
